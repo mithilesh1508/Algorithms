a8541087e6333dd0450f136864fe84914551e7b3
Mithilesh Kumar <mithilesh.kmr786@gmail.com>
2022-05-11 00:03:15 +0530

    Added generic for insertion sort and sort array without swap

--- InsertionSort.playground/Contents.swift
+++ InsertionSort.playground/Contents.swift
@@ -1,6 +1,6 @@
 import UIKit
 
-var str = "Hello, playground"
+//Note: Insertion sort is a stable sort. A sort is stable when elements that have identical sort keys remain in the same relative order after sorting
 func insertionSort(_ array: [Int]) -> [Int] {
     var sortedArr = array
     for currentIndex in 1..<array.count {
@@ -15,3 +15,48 @@ func insertionSort(_ array: [Int]) -> [Int] {
 }
 let arr = [10,-2,4,2,8,27,1,5,17,0, 26]
 insertionSort(arr)
+
+// Insertion Sort without swap
+func insertionSortNoSwap(_ array: [Int])-> [Int] {
+    var sortedArr = array
+    for index in 1..<sortedArr.count{
+        var currentIndex = index
+        let tempValue = sortedArr[currentIndex]
+        while currentIndex>0 && tempValue < sortedArr[currentIndex - 1] {
+           sortedArr[currentIndex] = sortedArr[currentIndex - 1]
+            currentIndex -= 1
+        }
+        sortedArr[currentIndex] = tempValue
+    }
+    
+    return sortedArr
+}
+
+let arr2 = [10,2,4,2,8,27,1,5,17,0, 26]
+insertionSortNoSwap(arr2)
+
+
+//Generic function
+func insertionSortGeneric<T>(_ array: [T], _ isOrderedBefore: (T, T) -> Bool) -> [T] {
+    var sortedArr = array
+    for index in 1..<sortedArr.count {
+        var currentIndex = index
+        let temp = sortedArr[currentIndex]
+        while currentIndex>0 && isOrderedBefore(temp, sortedArr[currentIndex - 1]) {
+            //Instead of writing temp < sortedArr[currentIndex - 1], we call the isOrderedBefore() function.
+            sortedArr[currentIndex] = sortedArr[currentIndex - 1]
+            currentIndex -= 1
+        }
+        sortedArr[currentIndex] = temp
+    }
+
+    return sortedArr
+}
+let numbers = [ 10, -1, 3, 9, 2, 27, 8, 5, 1, 3, 0, 26 ]
+insertionSortGeneric(numbers, <)
+insertionSortGeneric(numbers, >)
+
+let strings = [ "b", "a", "d", "c", "e","f" ]
+insertionSortGeneric(strings, <)
+insertionSortGeneric(strings, >)
+
